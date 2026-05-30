dfa30d9787cda96f056c57e2f57515694a63aa87
syahirghariff <syahirghariff@gmail.com>
2026-05-30 11:12:32 +0800

    simple testing for chunk and embedding

---
+++ app/unit_test/test_text_chunk_embed.py
@@ -0,0 +1,34 @@
+from sentence_transformers import SentenceTransformer
+import unittest
+
+from app.model.embeddings import EmbeddingModel
+from app.model.section import Section
+from app.repository.textbook_repository import TextbookRepository
+from app.service.chunking import chunk_article, chunk_text
+
+
+class TestTextChunkEmbed(unittest.TestCase):
+    def test_text_embedding(self):
+
+        section = self._find_section_from_unittest()
+        section = Section(**section)
+
+        chunks1 = chunk_article(section.content, 1, 200)
+        print(chunks1)
+
+        chunks2 = chunk_text(section.content)
+        print(chunks2)
+
+        model = EmbeddingModel()
+        embedding = model.encode(chunks1)
+        print('Embedding: ', embedding.shape)
+
+
+    def _find_section_from_unittest(self) -> Section:
+        textbook_repo = TextbookRepository()
+        section = textbook_repo.find_section_by_id('6a0dd06bc923557f95b2a76f')
+        print(section)
+        return section
+
+if __name__ == "__main__":
+    unittest.main()
--- app/unit_test/test_textbook_repository.py
+++ app/unit_test/test_textbook_repository.py
@@ -1,6 +1,4 @@
 import unittest
-import uuid
-from uuid import UUID
 
 from bson import ObjectId
 
