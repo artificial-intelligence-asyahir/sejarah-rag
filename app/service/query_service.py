570ecbc2793d34f6591179c65e71d84e10c221e5
syahirghariff <syahirghariff@gmail.com>
2026-06-22 12:24:12 +0800

    exploring ragas

--- app/service/query_service.py
+++ app/service/query_service.py
@@ -1,3 +1,9 @@
+from langchain_ollama import ChatOllama, OllamaEmbeddings
+from openai import AsyncOpenAI, OpenAI
+from ragas import Dataset, experiment
+from ragas.metrics import DiscreteMetric
+from ragas.llms import llm_factory
+import dataclasses
 from ollama import chat
 from ollama import ChatResponse
 from qdrant_client.http.models import QueryResponse
@@ -14,20 +20,11 @@ def _retrieve(query: str):
     embeddings = EmbeddingModel().encode(query).tolist()
     search_results: QueryResponse = vector_repo.search_vector(embeddings)
     results = [point.payload['text'] for point in search_results.points]
-    sumber = [_get_citations(point.payload['book_id'], point.payload['chapter_id'], point.payload['section_id']) for point in search_results.points]
-    return results, sumber
+    references = [_get_citations(point.payload['book_id'], point.payload['chapter_id'], point.payload['section_id']) for point in search_results.points]
+    return results, references
 
 def _rag_query(question: str):
-    context, sumber = _retrieve(question)
-
-    citation = "\n".join(
-        f"""Book Title: {c.book_title}
-    Author: {c.book_author}
-    Chapter: {c.chapter_no} - {c.chapter_title}
-    Section: {c.section_no} - {c.section_title}
-    """
-        for c in sumber
-    )
+    context, references = _retrieve(question)
 
     prompt = f"""
     [LANGUAGE CONSTRAINT]
@@ -37,10 +34,6 @@ def _rag_query(question: str):
     Anda merupakan skema jawapan bagi matapelajaran Sejarah di Sekolah Menengah Kebangsaan Malaysia.
     Sila berikan jawapan yang mudah dibaca dan mudah dipahami.
     
-    [SOURCE] 
-    Include the source of the information you use in your response.
-    {citation}
-
     Context:
     {chr(10).join(context)}
 
@@ -48,23 +41,23 @@ def _rag_query(question: str):
 
     Answer:"""
 
-    return prompt
+    return prompt, context, references
 
 def answer(question: str):
-    prompt = _rag_query(question)
+    prompt, context, references = _rag_query(question)
     response: ChatResponse = chat(model="gemma4", messages=[
         {"role": "user",
          "content": prompt}
     ])
 
-    return response.message.content
+    return response.message.content, context, references
 
 def _get_citations(book_id: str, chapter_id: str, section_id: str):
     book = textbook_repo.find_book_by_id(book_id)
     chapter = textbook_repo.find_chapter_by_id(chapter_id)
     section = textbook_repo.find_section_by_id(section_id)
 
-    return Citation(
+    citation = Citation(
         book_title=book['title'],
         book_author=book['author'],
         chapter_title=chapter['title'],
@@ -73,7 +66,84 @@ def _get_citations(book_id: str, chapter_id: str, section_id: str):
         section_no=section['section_no']
     )
 
+    return dataclasses.asdict(citation)
+
+# def ragas_evaluation():
+#     eval_llm = ChatOllama(model="gemma4")
+#     eval_embedding = OllamaEmbeddings(model="all-MiniLM-L6-v2")
+#
+#
+#     pass
+
+@experiment()
+async def run_experiment(row):
+
+    my_metric = DiscreteMetric(
+        name="correctness",
+        prompt="Check if the response contains points mentioned from the grading notes and return 'pass' or 'fail'.\nResponse: {response} Grading Notes: {grading_notes}",
+        allowed_values=["pass", "fail"],
+    )
+
+    client = OpenAI(
+        api_key="ollama",  # Ollama doesn't require a real key
+        base_url="http://localhost:11434/v1"
+    )
+    llm = llm_factory("gemma4", provider="openai", client=client)
+
+    score = my_metric.score(
+        llm=llm,
+        response=row['response'],
+        grading_notes=row["grading_notes"]
+    )
+
+    experiment_view = {
+        **row,
+        "response": sample['response'],
+        "score": score.value
+    }
+    print(experiment_view)
+    return experiment_view
+
+
 if __name__ == "__main__":
-    jawapan = answer("Apa itu sejarah?")
-    print(jawapan)
+    import asyncio
+
+
+
+    sample = {
+        "question" : "Zaman Prasejarah merupakan zaman sebelum manusia mengetahui dan mengenali tulisan. Zaman ini terbahagi kepada dua, iaitu Zaman Batu dan Zaman Logam, Senaraikan tiga tahap Zaman Batu dan terangkan Zaman Batu Tersebut",
+        "grading_notes": """
+        1.Zaman Paleolitik, 2.Zaman Mesolitik, 3.Zaman Neolitik. 
+        ## Zaman Paleolitk 
+        - Manusia perlu meneruskan kelangsungan hidup dengan menggunakan teknologi batu yang serba ringkas
+        - Menggunakan peralatan daripada tulang binatang dalam aktiviti harian
+        
+        ## Zaman Mesolitik 
+        - Memburu binatang dan menangkap ikan sebagai sumber makanan 
+        
+        ## Zaman Neolitik 
+        - Perkembangan teknologi dan corak kehidupan lebih baik 
+        - Wujud kawasan petempatan yang mengamalkan aktiviti bercucuk tanam, membuat tembikar dan menternak binatang
+        """
+    }
+
+    response, context, references = answer(sample['question'])
+    sample["contexts"] = context
+    sample["response"] = response
+    sample["references"] =  references
+
+    # evaluation_dataset = EvaluationDataset(list(sample))
+    #
+    # result = evaluate(dataset=evaluation_dataset, metrics=[LLMContextRecall(), Faithfulness(), FactualCorrectness()],
+    #                   llm=evaluator_llm)
+    # result
+
+    dataset = Dataset(name="test_dataset", backend="local/csv", root_dir=".")
+    dataset.append(sample)
+    dataset.save()
+
+    _ = asyncio.run(run_experiment.arun(dataset, name="ragas_evaluation"))
+
+
+    print (sample)
 
